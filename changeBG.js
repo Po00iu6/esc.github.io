e95bfd0dbc131b7c6c461b07a51bf634a4d5c1a4
Po00iu6 <devhming@outlook.com>
2024-11-26 00:01:17 +0800

    2024/11/26

---
+++ changeBG.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("BGButton");
+    const toggleDiv = document.getElementById("BGdiv");
+    let isDivVisible = true; // 跟踪div的当前可见性状态
+
+    toggleButton.addEventListener("click", function() {
+        // 切换div的显示状态
+        if (isDivVisible) {
+            toggleDiv.style.display = "none";
+            toggleButton.style.backgroundColor="#D3BA80";
+        } else {
+            toggleDiv.style.display = "block";
+            toggleButton.style.backgroundColor="#F9DB97";
+        }
+        // 更新状态
+        isDivVisible = !isDivVisible;
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("BGMusicButton");
+    const toggleDiv = document.getElementById("dd");
+    let bef = document.getElementById("dd").volume;
+    let isDivVisible = false; // 跟踪div的当前可见性状态
+
+    toggleButton.addEventListener("click", function() {
+        // 切换div的显示状态
+        if (isDivVisible) {
+            toggleDiv.volume=bef;
+            toggleButton.style.backgroundColor="#F9DB97";
+        } else {
+            toggleDiv.volume=0;
+            toggleButton.style.backgroundColor="#D3BA80";
+        }
+        // 更新状态
+        isDivVisible = !isDivVisible;
+    });
+});
--- 试验场/demo.js
+++ 试验场/demo.js
@@ -1,23 +1,16 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggleButton");
+    const toggleDiv = document.getElementById("toggleDiv");
+    let isDivVisible = false; // 跟踪div的当前可见性状态
 
-async function fetchImageUrls(url) {
-    try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
-        const imageUrls = [];
-        $('img').each((index, element) => {
-            const imgUrl = $(element).attr('src');
-            if (imgUrl) {
-                imageUrls.push(imgUrl);
-            }
-        });
-        return imageUrls;
-    } catch (error) {
-        console.error('Error fetching image URLs:', error);
-    }
-}
-
-// 示例调用
-fetchImageUrls('https://www.miyoushe.com/ys/article/59166853')
-    .then(imageUrls => console.log(imageUrls));
+    toggleButton.addEventListener("click", function() {
+        // 切换div的显示状态
+        if (isDivVisible) {
+            toggleDiv.style.display = "none";
+        } else {
+            toggleDiv.style.display = "block";
+        }
+        // 更新状态
+        isDivVisible = !isDivVisible;
+    });
+});
