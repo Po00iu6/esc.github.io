9b5b11d9440cf12f9ae40036522a65d90e676255
Po00iu6 <devhming@outlook.com>
2025-02-11 12:58:04 +0800

    2025/2/11

--- WHKScore/script.js
+++ WHKScore/script.js
@@ -1,6 +1,19 @@
 const ctx = document.getElementById('scoreChart').getContext('2d');
 
-let labels = ['2024年10月月考','2025年1月期末考试']
+let labels = [
+    '2024年10月月考',
+    '2025年1月期末考试'
+]
+
+let whkzcj = [0,352];
+let yw = [0,79];
+let sx = [0,45];
+let yy = [0,81];
+let wl = [0,28];
+let hf = [0,55];
+let sf = [0,64];
+
+
 
 const chart = new Chart(ctx, {
     type: 'line',
@@ -8,7 +21,7 @@ const chart = new Chart(ctx, {
         labels: labels,
         datasets: [{
             label: '文化课总成绩',
-            data: [0,352],
+            data: whkzcj,
             fill: true,
             backgroundColor: ['rgba(246, 255, 0, 0)'],
             borderColor: 'rgb(0, 153, 255)',
@@ -17,55 +30,55 @@ const chart = new Chart(ctx, {
             tension: 0.1
         },{
             label: '语文',
-            data: [0,79],
+            data: yw,
             fill: true,
             backgroundColor: ['rgba(246, 255, 0, 0)'],
-            borderColor: 'rgba(0, 192, 147, 0.4)',
+            borderColor: 'rgba(114, 0, 0, 0.4)',
             borderWidth: 2,
             pointHoverRadius: 5, //设置鼠标移动上去后圆点半径
             tension: 0.1
         },{
             label: '数学',
-            data: [0,45],
+            data: sx,
             fill: true,
             backgroundColor: ['rgba(246, 255, 0, 0)'],
-            borderColor: 'rgba(0, 192, 147, 0.4)',
+            borderColor: 'rgba(0, 106, 255, 0.4)',
             borderWidth: 2,
             pointHoverRadius: 5, //设置鼠标移动上去后圆点半径
             tension: 0.1
         },{
             label: '英语',
-            data: [0,81],
+            data: yy,
             fill: true,
             backgroundColor: ['rgba(246, 255, 0, 0)'],
-            borderColor: 'rgba(0, 192, 147, 0.4)',
+            borderColor: 'rgba(216, 20, 255, 0.4)',
             borderWidth: 2,
             pointHoverRadius: 5, //设置鼠标移动上去后圆点半径
             tension: 0.1
         },{
             label: '物理',
-            data: [0,28],
+            data: wl,
             fill: true,
             backgroundColor: ['rgba(246, 255, 0, 0)'],
-            borderColor: 'rgba(0, 192, 147, 0.4)',
+            borderColor: 'rgba(14, 255, 30, 0.4)',
             borderWidth: 2,
             pointHoverRadius: 5, //设置鼠标移动上去后圆点半径
             tension: 0.1
         },{
             label: '化赋',
-            data: [0,55],
+            data: hf,
             fill: true,
             backgroundColor: ['rgba(246, 255, 0, 0)'],
-            borderColor: 'rgba(0, 192, 147, 0.4)',
+            borderColor: 'rgba(32, 0, 192, 0.4)',
             borderWidth: 2,
             pointHoverRadius: 5, //设置鼠标移动上去后圆点半径
             tension: 0.1
         },{
             label: '生赋',
-            data: [0,64],
+            data: sf,
             fill: true,
             backgroundColor: ['rgba(246, 255, 0, 0)'],
-            borderColor: 'rgba(0, 192, 147, 0.4)',
+            borderColor: 'rgba(119, 177, 1, 0.4)',
             borderWidth: 2,
             pointHoverRadius: 5, //设置鼠标移动上去后圆点半径
             tension: 0.1
